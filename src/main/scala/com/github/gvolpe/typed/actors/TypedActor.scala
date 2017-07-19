3a7a018163d72b6096b222711a436c389dd2ea81
Gabi Volpe <volpegabriel@gmail.com>
2017-07-19 09:53:30 +0200

    Sorting out type erasure using ClassTag. It has to be an abstract class though...

--- src/main/scala/com/github/gvolpe/typed/actors/TypedActor.scala
+++ src/main/scala/com/github/gvolpe/typed/actors/TypedActor.scala
@@ -2,23 +2,25 @@ package com.github.gvolpe.typed.actors
 
 import akka.actor.{Actor, ActorRef}
 
+import scala.reflect.ClassTag
+
 /**
   * Simple [[TypedActor]] that gives any class implementing it the power to have typed messages making
   * proper use of the compiler for type check exhaustiveness by just using a typed [[Function1]].
   *
-  * For convenience use this trait instead of using directly [[Actor]] unless you have a good reason.
+  * For convenience use this abstract class instead of using directly [[Actor]].
   * */
-trait TypedActor[A] extends Actor {
+abstract class TypedActor[A : ClassTag] extends Actor {
   type TypedReceive = A => Unit
   def typedReceive: TypedReceive
 
-  private def liftToPF[X <: Y, W, Y](f: Function[X, W]): PartialFunction[Y, W] =
+  private def liftToPF[X <: Y : ClassTag, W, Y](f: Function[X, W]): PartialFunction[Y, W] =
     new PartialFunction[Y, W] {
-      override def isDefinedAt(x: Y): Boolean = x.isInstanceOf[X @unchecked]
+      override def isDefinedAt(x: Y): Boolean = implicitly[ClassTag[X]].runtimeClass.isAssignableFrom(x.getClass)
       override def apply(v1: Y): W = f(v1.asInstanceOf[X])
     }
 
-  override def receive: Receive = liftToPF(typedReceive)
+  override def receive: Receive = liftToPF[A, Unit, Any](typedReceive)
 }
 
 class TypedActorRef[A](ref: ActorRef) {
